8972a5a033727cb72d14d3dbb6be5114724a5db1
Sagar Gavhane <sgavhane70@gmail.com>
2024-11-15 21:57:37 +0530

    chore: rename to Client.java

--- src/main/java/com/patterns/creational/singleton/Client.java
+++ src/main/java/com/patterns/creational/singleton/Client.java
@@ -1,6 +1,6 @@
 package com.patterns.creational.singleton;
 
-public class Main {
+public class Client {
     public static void main(String[] args) {
         Singleton instance = Singleton.getInstance("demo value");
         System.out.println(instance.getValue());
